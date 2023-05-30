390e0baf0339bb46f57bbe3f4615fbb7637e23a7
Marcos Santos <marcos.santos@locximoveis.com.br>
2023-05-30 10:30:37 -0300

    feat: global font family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { globalCss } from "./stitches.config";
 export const globalStyles = globalCss({
   'html': {
     fontSize: '62.5%',
+    fontFamily: '$system'
   },
   '*': {
     margin: 0,
